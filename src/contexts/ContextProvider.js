2a26205cccd379c61c046a1e3d9b2541354725fc
pgm-thabisadingani <71275598+pgm-thabisadingani@users.noreply.github.com>
2022-10-21 13:45:13 +0200

    added core pages and components

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,46 @@
+import React, { createContext, useContext, useState } from "react";
+
+const StateContext = createContext();
+
+const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
+};
+
+// values: will be passed through the application
+// return children
+
+export const ContextProvider = ({ children }) => {
+  // application logic
+
+  const [activeMenu, setActiveMenu] = useState(true);
+
+  const [isClicked, setIsClicked] = useState(initialState);
+
+  //initialState returns an object, thus use a spread operator, and targetd the value you want to change [value] : boolean
+
+  const handleClick = (clicked) => {
+    setIsClicked({ ...initialState, [clicked]: true });
+  };
+
+  //passing values to the state context
+  return (
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
+    <StateContext.Provider
+      value={{
+        activeMenu,
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+// get the data from the context using the context
+export const useStateContext = () => useContext(StateContext);

588da327a2e634acd4d94a62141e062cc256095b
pgm-thabisadingani <71275598+pgm-thabisadingani@users.noreply.github.com>
2022-11-26 15:42:16 +0100

    charts

--- src/pages/Charts/Stacked.jsx
+++ src/pages/Charts/Stacked.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 
-import { Header, LineChart } from "../../components";
+import { Header, Stacked as StackedChart } from '../../components';
 const Line = () => {
   return (
     <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
-      <Header title="Inflation Rate" category="Chart" />
+      <Header category="Stacked" title="Revenue Breakdown" />
       <div className="w-full">
-        <LineChart />
+        <StackedChart />
       </div>
     </div>
   );
